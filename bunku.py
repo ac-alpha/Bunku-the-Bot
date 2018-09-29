a2ec394daca3b1bae7de6e1dac415b159cc9893d
Ashutosh Chaubey <ashutoshchaubey2999@gmail.com>
2018-09-29 19:31:18 +0530

    Added extra cancel and leave features

--- bunku.py
+++ bunku.py
@@ -35,15 +35,6 @@ Type the following and make me help you:\n\
 @Bunku extra class <course-code> : to report about an extra class\n\
 @Bunku attendancerecord : to show your attendance record"
 
-
-# tt=[{"t8_9":"","t9_10":"min106","t10_11":"csn221","t11_12":"csn291","t12_1":"","t2_3":"","t3_4":"min106","t4_5":"ecn203","t5_6":"hss01"},\
-# {"t8_9":"hss01","t9_10":"min106","t10_11":"ecn203","t11_12":"csn291","t12_1":"","t2_3":"","t3_4":"csn291","t4_5":"min106","t5_6":""},\
-# {"t8_9":"","t9_10":"ecn203","t10_11":"csn221","t11_12":"","t12_1":"","t2_3":"","t3_4":"csn261","t4_5":"","t5_6":""},\
-# {"t8_9":"hss01","t9_10":"min106","t10_11":"ecn203","t11_12":"csn221","t12_1":"csn291","t2_3":"csn221","t3_4":"","t4_5":"","t5_6":""},\
-# {"t8_9":"","t9_10":"","t10_11":"","t11_12":"","t12_1":"","t2_3":"","t3_4":"","t4_5":"","t5_6":""}]
-# leaverecord = {"min106":3,"csn221":2,"csn291":2,"csn261":3,"ecn203":1,"hss01":0}
-# workingdays = {"startsession":"2018-07-23","endsession":"2018-11-10","min106":40,"csn221":40,"csn291":20,"csn261":20,"ecn203":40,"hss01":30}
-
 def get_bot_converter_response(message: Dict[str, str], bot_handler: Any) -> str:
     content = message['content']
     original_sender = message['sender_email']
@@ -69,85 +60,36 @@ def get_bot_converter_response(message: Dict[str, str], bot_handler: Any) -> str
         msg_response+="-----------"
     
     words = content.lower().split()
-
-    # if(len(words)==1):
-
-    #     if(words[0].strip()=="left"):
-    #         currTime = datetime.datetime.now().time().hour
-    #         if(8<=currTime<=17):
-    #             currDay = datetime.datetime.today().weekday();
-    #             dayText=tt[currDay]
-    #             dayTimeTableList = dayText.split("~");
-    #             currSubCode = dayTimeTableList[currTime-8].strip()
-    #             if(currSubCode==""):
-    #                 msg_response="No classes now. Type \"@bunku help\""
-    #             else:
-    #                 msg_response="Leave for "+currSubCode+" recorded\n"
-    #                 bot_handler.storage.put("leaves",currSubCode+"-1")
-    #         else:
-    #             msg_response="No classes now. Type \"@bunku help\"" 
-
-    #     elif(words[0].strip()=="startdate"):
-    #         startDate=bot_handler.storage.get("startDate")
-    #         msg_response= startDate+" is the start date of the session."
-
-    #     elif(words[0].strip()=="enddate"):
-    #         endDate=bot_handler.storage.get("endDate")
-    #         msg_response = endDate+" is the start date of the session."
-        
-    #     elif(words[0].strip()=="startrecording"):
-    #         bot_handler.storage.put("leaveRecord",convertRecordToString(leaverecord))
-    #         msg_response += "Started recording the leaves now... Happy bunking!"
-        
-    #     elif(words[0].strip()=="rawrecords"):
-    #         rawrecord = bot_handler.storage.get("leaveRecord")
-    #         msg_response += rawrecord
-
-
-    # elif(len(words)==2):
-
-    #     if(words[0]=="startsession"):
-    #         date=words[1].strip()
-    #         if(len(date)==10 and date[0].isdigit() and date[1].isdigit() and date[3].isdigit() and date[4].isdigit() and date[6].isdigit() and date[7].isdigit() and\
-    #             date[8].isdigit() and date[9].isdigit() ):
-    #             bot_handler.storage.put("startDate",date)
-    #             msg_response=date+" is set as start date."
-    #         else:
-    #             msg_response = "Enter date in required format"
-
-    #     elif(words[0]=="endsession"):
-    #         date=words[1].strip()
-    #         if(len(date)==10 and date[0].isdigit() and date[1].isdigit() and date[3].isdigit() and date[4].isdigit() and date[6].isdigit() and date[7].isdigit() and\
-    #             date[8].isdigit() and date[9].isdigit() ):
-    #             bot_handler.storage.put("endDate",date)
-    #             msg_response=date+" is set as end date."
-    #         else:
-    #             msg_response = "Enter date in required format"
-    #     else:
-    #         msg_response="I don't know how to respond to that. Type \"@bunku help\""
+    if(len(words)==3):
+        courses = list(graphql.getLeaveStats.keys())
+        if(words[0].lower().strip()=="left" and words[2].lower().strip(=="class")):
+            courseCode = words[1].lower()
+            if courseCode in courses:
+                graphql.updateLeave(original_sender,courseCode)
+                msg_response+="Leave for "+courseCode+" recorded successfully. Try \"@Bunku attendancerecord\" for leave stats"
+            else:
+                msg_response+="Sorry! I don't know which course is that. Try \"@Bunku help\""
+        elif(words[1].lower().strip()=="class" and words[2].lower().strip()=="cancelled"):
+            courseCode = words[0].lower().strip()
+            if courseCode in courses:
+                graphql.cancelClass(courseCode)
+                msg_response+="Class of "+courseCode+" has been cancelled."
+            else:
+                msg_response+="Sorry! I don't know which course is that. Try \"@Bunku help\""
+        elif(words[0].lower().strip()=="extra" and words[1].lower().strip()=="class"):
+            courseCode = words[2].lower().strip()
+            if courseCode in courses:
+                graphql.extraClass(courseCode)
+                msg_response+="Extra Class of "+courseCode+" has been recorded."
+            else:
+                msg_response+="Sorry! I don't know which course is that. Try \"@Bunku help\""
+        else:
+            msg_response+="Sorry! I don't know how to respond to that. Try \"@Bunku help\""
+    else:
+        msg_response+="Sorry! I don't know how to respond to that. Try \"@Bunku help\""
                 
     return msg_response
 
-def convertRecordToString(leaverecord):
-    recordString=""
-    for course in leaverecord:
-        for elem in course:
-            recordString+=str(elem)+"~"
-        recordString+="\b$"
-    recordString+="\b"
-    return recordString
-
-def convertStringToRecord(recordString):
-    recordString=recordString[:len(recordString)-1]
-    recordList=recordString.split("$")
-    temp=[]
-    for record in recordList:
-        record=record.strip().split("~")
-        record[1]=int(record[1])
-        record[2]=int(record[2])
-        record[3]=int(record[3])
-        temp.append(record)
-    return temp
 
 def daysTillNow(courseCode):
     startDate = graphql.getDaysInfo()["startsession"]    #workingdays["startsession"]
